5f97f533dd5d686f1b3684187bed1b1cfd5ad39c
oldnic <zaid.alaggel@gmail.com>
2020-04-17 20:35:48 +0300

    Product model and InMemory Product

---
+++ BlankShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyShop.Core.Models {
+    public class Product {
+        public string Id { get; set; }
+
+        [DisplayName("Product Name")]
+        [StringLength(100)]
+        public string Name { get; set; }
+        public string Description { get; set; }        
+        
+        [Range(0, 10000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+        public Product() {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
---
+++ BlankShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory {
+    class ProductRepository {
+        ObjectCache objectCache = MemoryCache.Default;
+        List<Product> products;
+        public ProductRepository() {
+            products = objectCache["products"] as List<Product>;
+            
+            if (products == null) {
+                products = new List<Product>();
+            }
+        }
+        public void Commit() {
+            objectCache["products"] = products;
+        }
+        public void Insert(Product product) {
+            products.Add(product);
+        }
+        public void Update(Product product) {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if (productToUpdate != null) {
+                productToUpdate = product;
+            } else {
+                throw new Exception("Product not found");
+            }
+        }
+        public Product Find(string Id) {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null) {
+                return product;
+            } else {
+                throw new Exception("Product not found");
+            }
+        }
+        public IQueryable<Product> Collections() { 
+            return products.AsQueryable();
+        }
+        public void Delete(string Id) {
+            Product productToDelete = products.Find(p => p.Id == Id);
+
+            if (productToDelete != null) {
+                products.Remove(productToDelete);
+            } else {
+                throw new Exception("Product not found");
+            }
+        }
+    }
+}
